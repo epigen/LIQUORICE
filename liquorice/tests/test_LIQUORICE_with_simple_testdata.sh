b45b13fb0b7a55ed359cec4c0b2e7d2df332a2ff
Peter Peneder <peter.peneder@ccri.at>
2021-09-16 17:09:57 +0200

    Added unzipping and download of .bai file to usage example

--- liquorice/tests/test_LIQUORICE_with_simple_testdata.sh
+++ liquorice/tests/test_LIQUORICE_with_simple_testdata.sh
@@ -2,12 +2,14 @@ N_CPUS=1
 
 # download and unzip the reference genome and reference mappability file
 wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/p12/hg38.p12.fa.gz
-gunzip hg38.fa.gz
+gunzip hg38.p12.fa.gz
 wget https://github.com/epigen/LIQUORICE/raw/master/liquorice/data/hg38.p12.fa.fai
 wget https://github.com/epigen/LIQUORICE/raw/master/liquorice/data/hg38.fa.mappability_100bp.subsetted_for_testdata.bw
 
 # download .bam file of a healthy control liquid biopsy sample (pre-processed to keep the size small)
 wget https://github.com/epigen/LIQUORICE/raw/master/liquorice/data/Ctrl_17_testdata.bam
+wget https://github.com/epigen/LIQUORICE/raw/master/liquorice/data/Ctrl_17_testdata.bam.bai
+
 
 # download .bed file for universally accessible DHSs
 wget https://github.com/epigen/LIQUORICE/raw/master/liquorice/data/universal_DHSs.bed
