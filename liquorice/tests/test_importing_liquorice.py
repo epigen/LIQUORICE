c861a477409076834ef344101f8ece97bc870010
Peter Peneder <peter.peneder@ccri.at>
2021-09-16 11:50:00 +0200

    Adapted testin in meta.yaml; added github action to create tarballs on push

--- liquorice/tests/test_importing_liquorice.py
+++
@@ -1,7 +0,0 @@
-import liquorice
-from liquorice.utils import Workflows
-import liquorice.LIQUORICE
-import subprocess
-
-subprocess.check_call(["LIQUORICE","--help"])
-subprocess.check_call(["LIQUORICE_summary","--help"])
