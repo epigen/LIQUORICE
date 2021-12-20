72f054e788735e62105e34b6ac1e73a08acffc87
Peter Peneder <peter.peneder@ccri.at>
2021-12-20 15:32:08 +0100

    Update dockerfile to v0.5.4; add google verification to docs Also: Updated meta.yaml

--- dockerfile
+++ dockerfile
@@ -1,3 +1,3 @@
 FROM condaforge/mambaforge:4.9.2-5 as conda
 
-RUN mamba install -c bioconda -c conda-forge liquorice==0.5.3 ray-core
+RUN mamba install -c bioconda -c conda-forge liquorice==0.5.4 ray-core
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, Peter Peneder'
 author = 'Peter Peneder'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.3'
+release = '0.5.4'
 
 
 # -- General configuration ---------------------------------------------------
@@ -65,3 +65,5 @@ html_logo = '../img/liquorice_logo_fitted_transparent.png'
 html_theme_options = {
     'logo_only': True,
 }
+
+html_extra_path = "google25ad18a36b820a71.html"
